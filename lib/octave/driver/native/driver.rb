64728164734f4a636ade03e5ba75512eafbe2c93
mkfs <semper.non.sequitur@gmail.com>
2016-03-24 21:10:16 -0400

    Moved API.exit call into an ObjectSpace finalizer. This eliminates the need for the user to invoke exit() themselves (maintaining backwards compatibility) but does mean that the ruby version must support this method (Ruby 2.0). Users of older version of Ruby are welcome to submit patches e.g. with an at_exit handler

--- lib/octave/driver/native/driver.rb
+++ lib/octave/driver/native/driver.rb
@@ -6,6 +6,12 @@ end
 
 module Octave ; module Driver ; module Native
   class Driver
+
+    def initialize()
+      # force Octave::clean_up_and_exit to be called
+      ObjectSpace.define_finalizer(self, proc {|id| API.exit })
+    end
+
     def feval(function_name, *args)
       API.feval function_name, args
     end
@@ -17,9 +23,5 @@ module Octave ; module Driver ; module Native
     def get_variable(name)
       API.get_variable(name)
     end  
-
-    def exit
-      API.exit
-    end
   end
 end ; end ; end
